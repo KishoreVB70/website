2a9d5479aa95ee718570f492542027f153d3983a
Dario Aschero <dario.aschero@gmail.com>
2024-10-07 18:29:50 +0200

    font + design fixes

--- components/card.tsx
+++ components/card.tsx
@@ -11,7 +11,7 @@ export default function Card({ title, description, flip = false }: CardProps) {
     <div className={`grid grid-cols-1 md:grid-cols-2 gap-6 px-6 py-32 border-b border-border ${flip ? 'md:grid-flow-col' : ''}`}>
       <div className={`aspect-square bg-muted rounded-xl border border-border ${flip ? 'md:order-last' : ''}`}></div>
       <div className={`flex items-center justify-center ${flip ? 'md:order-first' : ''}`}>
-        <div className="md:max-w-72 flex flex-col">
+        <div className="md:max-w-72 flex flex-col gap-2">
           <h2 className="text-md font-bold">{title}</h2>
           <p className="text-md">{description}</p>
         </div>
--- components/cta-top.tsx
+++ components/cta-top.tsx
@@ -10,7 +10,7 @@ export default function CTATop() {
         </p>
       </div>
       <div>
-        <a href="/" className="flex flex-row gap-1 items-center hover:text-violet-600">
+        <a href="/" className="flex flex-row gap-1 items-center hover:text-violet-600 font-bold">
           <span className="relative flex h-3 w-3">
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-violet-400 opacity-75"></span>
             <span className="relative inline-flex rounded-full h-3 w-3 bg-violet-500"></span>
