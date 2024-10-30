fffa0022ca608fac44cebb80c46e5549eac938da
Jonath-z <jonathanzihindula95@gmail.com>
2024-10-30 10:27:46 +0200

    feat: add flag for static build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const staticBuildEnabled = process.env.STATIC_BUILD_ENABLED === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: staticBuildEnabled ? "export" : undefined,
 };
 
 export default nextConfig;
